7bbef1135225ae81548ecd5fe87d78fef8e8e9ad
Federico Viotti <fedeviotti@gmail.com>
2022-12-04 14:46:22 +0100

    chore: add garage router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
+import { garageRouter } from "./garage";
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
+  garage: garageRouter,
 });
 
 // export type definition of API
