3fcde026860023a2eb175a92cbf102286994c32e
Federico Viotti <fedeviotti@gmail.com>
2022-12-07 22:26:39 +0100

    refactor(toast): change default options

--- src/constants/defaultToastOptions.ts
+++ src/constants/defaultToastOptions.ts
@@ -1,7 +1,7 @@
 import type { UseToastOptions } from "@chakra-ui/react";
 
 export const defaultToastOptions: Pick<UseToastOptions, "position" | "duration" | "isClosable"> = {
-  position: "top",
-  duration: 5000,
+  position: "bottom-right",
+  duration: 3500,
   isClosable: true,
 };
