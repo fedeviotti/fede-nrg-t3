67e1981aae6f82ef2f9786cb9534fdc25fd3c979
Federico Viotti <fedeviotti@gmail.com>
2022-12-11 16:11:54 +0100

    fix: navbar ui fixes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -36,8 +36,7 @@ const Navbar = () => {
     >
       <Flex
         width={PAGE_WIDTH}
-        px="24px"
-        py="12px"
+        py={4}
         justifyContent="space-between"
       >
         <Image
