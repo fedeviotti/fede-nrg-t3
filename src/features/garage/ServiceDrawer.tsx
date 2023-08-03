c2bae72ce737ae899b023ca89e3656faa34f80d9
Federico Viotti <fedeviotti@gmail.com>
2023-08-03 11:36:07 +0200

    chore(vehicle): add form in service drawer

--- src/features/garage/ServiceDrawer.tsx
+++ src/features/garage/ServiceDrawer.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import {
+  Box,
   Button, Drawer,
   DrawerBody,
   DrawerCloseButton,
   DrawerContent,
   DrawerFooter,
   DrawerHeader,
-  DrawerOverlay,
-  Input, Stack,
+  DrawerOverlay, Flex, Input,
+  Stack,
 } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
+import {
+  ErrorMessage, Field, Form, Formik,
+} from "formik";
 
 type Props = {
   isOpen: boolean;
-
   onClose: () => void;
   btnRef: React.RefObject<HTMLButtonElement>;
+};
+
+const INITIAL_VALUES = {
+  name: "",
+  description: "",
+  price: 0,
 };
 
 export const ServiceDrawer = ({ isOpen, onClose, btnRef }: Props) => {
@@ -33,7 +42,27 @@ export const ServiceDrawer = ({ isOpen, onClose, btnRef }: Props) => {
         <DrawerHeader>{t("garage.vehicle.service_drawer.title")}</DrawerHeader>
 
         <DrawerBody>
-          <Input placeholder="Type here..." />
+          <Formik
+            initialValues={INITIAL_VALUES}
+            onSubmit={() => {}}
+          >
+            <Form>
+              <Flex direction="column" gap={4}>
+                <Box width={["xs", "md"]} height="64px">
+                  <Field as={Input} type="text" name="name" placeholder={t("garage.vehicle.service_drawer.create_form.name")} />
+                  <ErrorMessage component="div" name="name" />
+                </Box>
+                <Box width={["xs", "md"]} height="64px">
+                  <Field as={Input} type="text" name="description" placeholder={t("garage.vehicle.service_drawer.create_form.description")} />
+                  <ErrorMessage component="div" name="description" />
+                </Box>
+                <Box width={["xs", "md"]} height="64px">
+                  <Field as={Input} type="text" name="price" placeholder={t("garage.vehicle.service_drawer.create_form.price")} />
+                  <ErrorMessage component="div" name="price" />
+                </Box>
+              </Flex>
+            </Form>
+          </Formik>
         </DrawerBody>
 
         <DrawerFooter>
