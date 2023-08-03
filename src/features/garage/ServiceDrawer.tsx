e4865cf7c4cd2feb2a0f2378050f7de5c989a756
Federico Viotti <fedeviotti@gmail.com>
2023-08-03 15:57:40 +0200

    chore(service): add service form

--- src/features/garage/ServiceDrawer.tsx
+++ src/features/garage/ServiceDrawer.tsx
@@ -14,6 +14,7 @@ import { useTranslation } from "react-i18next";
 import {
   ErrorMessage, Field, Form, Formik,
 } from "formik";
+import * as yup from "yup";
 
 type Props = {
   isOpen: boolean;
@@ -29,6 +30,21 @@ const INITIAL_VALUES = {
 
 export const ServiceDrawer = ({ isOpen, onClose, btnRef }: Props) => {
   const { t } = useTranslation("common");
+  const validationSchema = React.useMemo(
+    () => yup.object().shape({
+      name: yup
+        .string()
+        .required(t("garage.vehicle.serviceDrawer.createForm.name.required") || "Error")
+        .min(3, t("garage.vehicle.serviceDrawer.createForm.name.minLength") || "Error"),
+      description: yup
+        .string(),
+      price: yup.number()
+        .required(t("garage.vehicle.serviceDrawer.createForm.price.required") || "Error")
+        .min(0, t("garage.vehicle.serviceDrawer.createForm.price.minValue") || "Error"),
+    }),
+    [t],
+  );
+
   return (
     <Drawer
       isOpen={isOpen}
@@ -38,41 +54,65 @@ export const ServiceDrawer = ({ isOpen, onClose, btnRef }: Props) => {
     >
       <DrawerOverlay />
       <DrawerContent>
-        <DrawerCloseButton />
-        <DrawerHeader>{t("garage.vehicle.serviceDrawer.title")}</DrawerHeader>
-
-        <DrawerBody>
-          <Formik
-            initialValues={INITIAL_VALUES}
-            onSubmit={() => {}}
-          >
+        <Formik
+          initialValues={INITIAL_VALUES}
+          onSubmit={() => {}}
+          validationSchema={validationSchema}
+          validateOnMount
+        >
+          {(isValid) => (
             <Form>
-              <Flex direction="column" gap={4}>
-                <Box width={["xs", "md"]} height="64px">
-                  <Field as={Input} type="text" name="name" placeholder={t("garage.vehicle.serviceDrawer.createForm.name")} />
-                  <ErrorMessage component="div" name="name" />
-                </Box>
-                <Box width={["xs", "md"]} height="64px">
-                  <Field as={Input} type="text" name="description" placeholder={t("garage.vehicle.serviceDrawer.createForm.description")} />
-                  <ErrorMessage component="div" name="description" />
-                </Box>
-                <Box width={["xs", "md"]} height="64px">
-                  <Field as={Input} type="text" name="price" placeholder={t("garage.vehicle.serviceDrawer.createForm.price")} />
-                  <ErrorMessage component="div" name="price" />
-                </Box>
-              </Flex>
-            </Form>
-          </Formik>
-        </DrawerBody>
+              <DrawerCloseButton />
+              <DrawerHeader>{t("garage.vehicle.serviceDrawer.title")}</DrawerHeader>
+              <DrawerBody>
+                <Flex direction="column" gap={4}>
+                  <Box width={["xs", "md"]} height="64px">
+                    <Field
+                      as={Input}
+                      type="text"
+                      name="name"
+                      placeholder={t("garage.vehicle.serviceDrawer.createForm.name.placeholder")}
+                    />
+                    <ErrorMessage component="div" name="name" />
+                  </Box>
+                  <Box width={["xs", "md"]} height="64px">
+                    <Field
+                      as={Input}
+                      type="text"
+                      name="description"
+                      placeholder={t("garage.vehicle.serviceDrawer.createForm.description.placeholder")}
+                    />
+                    <ErrorMessage component="div" name="description" />
+                  </Box>
+                  <Box width={["xs", "md"]} height="64px">
+                    <Field
+                      as={Input}
+                      type="text"
+                      name="price"
+                      placeholder={t("garage.vehicle.serviceDrawer.createForm.price.placeholder")}
+                    />
+                    <ErrorMessage component="div" name="price" />
+                  </Box>
+                </Flex>
+              </DrawerBody>
 
-        <DrawerFooter>
-          <Stack direction="row">
-            <Button variant="ghost" colorScheme="primary" onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme="primary">Save</Button>
-          </Stack>
-        </DrawerFooter>
+              <DrawerFooter>
+                <Stack direction="row">
+                  <Button variant="ghost" colorScheme="primary" onClick={onClose}>
+                    Cancel
+                  </Button>
+                  <Button
+                    type="submit"
+                    colorScheme="primary"
+                    isDisabled={!isValid}
+                  >
+                    Save
+                  </Button>
+                </Stack>
+              </DrawerFooter>
+            </Form>
+          )}
+        </Formik>
       </DrawerContent>
     </Drawer>
   );
