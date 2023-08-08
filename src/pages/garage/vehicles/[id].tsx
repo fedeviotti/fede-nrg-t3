7920e41ebf251a8cc2d3c9254b6d4073a06d9379
Federico Viotti <fedeviotti@gmail.com>
2023-08-08 13:48:33 +0200

    chore(service): service drawer improvements

--- src/pages/garage/vehicles/[id].tsx
+++ src/pages/garage/vehicles/[id].tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { trpc } from "utils/trpc";
-import { useIsAuthenticated } from "hooks/useIsAuthenticated";
+import { trpc } from "~/utils/trpc";
+import { useIsAuthenticated } from "~/hooks/useIsAuthenticated";
 import {
   Button,
   Flex,
@@ -13,7 +13,8 @@ import {
 } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import { AddIcon, ArrowBackIcon } from "@chakra-ui/icons";
-import { ServiceDrawer } from "features/garage/ServiceDrawer";
+import { ServiceDrawer } from "~/features/garage/ServiceDrawer";
+import { ServiceCard } from "~/features/garage/ServiceCard";
 
 const VehicleMaintenance = () => {
   const { t } = useTranslation("common");
@@ -22,13 +23,26 @@ const VehicleMaintenance = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef(null);
   const { id } = router.query;
-  const { data: vehicle, isLoading, isError } = trpc.garage.getVehicleById.useQuery(
+  const {
+    data: vehicle,
+    isLoading: isLoadingVehicle,
+    isError: isErrorVehicle,
+  } = trpc.garage.getVehicleById.useQuery(
     { id: Number(id) },
     { enabled: sessionData?.user !== undefined },
   );
+  const {
+    data: services,
+    isLoading: isLoadingServices,
+    isError: isErrorServices,
+  } = trpc.garage.getServicesByVehicleId.useQuery({
+    id: Number(id),
+  }, {
+    enabled: id !== undefined,
+  });
 
-  if (isError) return <Flex justifyContent="center">An error occurred.</Flex>;
-  if (isLoading) return <Flex justifyContent="center"><Spinner size="lg" /></Flex>;
+  if (isErrorVehicle || isErrorServices) return <Flex justifyContent="center">An error occurred.</Flex>;
+  if (isLoadingVehicle || isLoadingServices) return <Flex justifyContent="center"><Spinner size="lg" /></Flex>;
 
   return (
     <>
@@ -42,7 +56,6 @@ const VehicleMaintenance = () => {
           >
             {t("back")}
           </Button>
-          {/* onClick open Drawer */}
           <Button
             colorScheme="primary"
             ref={btnRef}
@@ -52,7 +65,7 @@ const VehicleMaintenance = () => {
             {t("garage.vehicle.card.service.ctaAdd")}
           </Button>
         </Stack>
-        <Stack direction="column" spacing="16px">
+        <Stack direction="column" spacing={5}>
           <Heading size="md">
             {vehicle?.name}
           </Heading>
@@ -60,10 +73,10 @@ const VehicleMaintenance = () => {
             {vehicle?.description}
           </Text>
           {/* Add Image  */}
-          {/* Add list services as table */}
+          {services.map((service) => (<ServiceCard key={service.id} service={service} />))}
         </Stack>
       </Stack>
-      <ServiceDrawer isOpen={isOpen} onClose={onClose} btnRef={btnRef} />
+      <ServiceDrawer isOpen={isOpen} onClose={onClose} btnRef={btnRef} vehicleId={Number(id)} />
     </>
   );
 };
