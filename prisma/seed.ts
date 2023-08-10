5945d64ec05744c0aaf722c9b6a32459f51075c6
Federico Viotti <fedeviotti@gmail.com>
2023-08-10 15:48:18 +0200

    chore(db): edit seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,7 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const bike = await prisma.vehicleType.create({
+  // VehicleTypes
+  await prisma.vehicleType.create({
     data: {
       name: "bike",
       description: "Bycicle",
@@ -15,53 +16,10 @@ async function main() {
       description: "Car",
     },
   });
-
-  // NOTE: add manually ownerId that is created when a user is created
-  const rockrider = await prisma.vehicle.create({
+  await prisma.vehicleType.create({
     data: {
-      name: "Rockrider ST 540",
-      description: "MTB",
-      typeId: bike.id,
-    },
-  });
-
-  const elops = await prisma.vehicle.create({
-    data: {
-      name: "Elops",
-      description: "City bike",
-      typeId: bike.id,
-    },
-  });
-
-  await prisma.vehicle.create({
-    data: {
-      name: "Giant",
-      description: "MTB Front Full Carbon",
-      typeId: bike.id,
-    },
-  });
-
-  await prisma.service.create({
-    data: {
-      name: "Manutenzione pedale",
-      description: "Fissaggio pedale dopo che si è staccato due volte",
-      vehicleId: rockrider.id,
-    },
-  });
-
-  await prisma.service.create({
-    data: {
-      name: "Cambio gomme",
-      description: "Messo gomme tubeless",
-      vehicleId: rockrider.id,
-    },
-  });
-
-  await prisma.service.create({
-    data: {
-      name: "Check annuale",
-      description: "Controllo annuale gratuito entro il primo anno",
-      vehicleId: elops.id,
+      name: "motorbike",
+      description: "Motorbike",
     },
   });
 }
