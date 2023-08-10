cc31c9eff4fc2881052ba47825e3063ec3f56383
Federico Viotti <fedeviotti@gmail.com>
2023-08-10 15:48:43 +0200

    chore(service): add types

---
+++ src/features/garage/types/serviceDurationOption.ts
@@ -0,0 +1,21 @@
+type DayValues = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 14 | 21 | 28;
+type MonthValues = 1 | 2 | 3 | 4 | 5 | 6 | 9;
+type YearValues = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+
+type DayAsDaysNumber = DayValues;
+type MonthAsDaysNumber = 30 | 60 | 90 | 120 | 150 | 180 | 270;
+type YearAsDaysNumber = 365 | 730 | 1095 | 1460 | 1825 | 2190 | 2555 | 2920 | 3285 | 3650;
+
+export type ServiceDurationOption = {
+  value: DayValues;
+  type: "day";
+  daysNumber: DayAsDaysNumber;
+} | {
+  value: MonthValues;
+  type: "month";
+  daysNumber: MonthAsDaysNumber;
+} | {
+  value: YearValues;
+  type: "year";
+  daysNumber: YearAsDaysNumber;
+};
